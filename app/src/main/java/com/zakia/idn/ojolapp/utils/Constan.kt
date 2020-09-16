d04d13e1d4a3b905489e2d85fe554c386c559403
FUJITSU <deanumainah124@gmail.com>
2020-09-16 14:51:59 +0700

    apa

--- app/src/main/java/com/zakia/idn/ojolapp/utils/Constan.kt
+++ app/src/main/java/com/zakia/idn/ojolapp/utils/Constan.kt
@@ -8,7 +8,7 @@ object Constan {
     val BaseUrlRoute = "https://maps.googleapis.com/maps/api/directions/"
     val BaseUrlFcm = "https://fcm.googleapis.com"
     val key = "key"
-//    val API_KEY = ""
+    val API_KEY = ""
 
-    val API_KEY = "AIzaSyCPy5d7fVtJN9U0cUM2Cj9i5_DNWG-9WtQ"
+//    val API_KEY = "AIzaSyCPy5d7fVtJN9U0cUM2Cj9i5_DNWG-9WtQ"
 }
